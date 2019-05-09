be8814c599b66f3972e9dcc36c46b3f3973103d2
yangpeng <aitexiaoy@163.com>
2019-05-09 19:23:33 +0800

    feat(main): 修复Bug

--- src/api/api.js
+++ src/api/api.js
@@ -1,20 +1,25 @@
 
+const qs = require('qs')
 const axios = require('axios')
 const { apiBaseUrl } = require('../utils/config.js')
 
 const instance = axios.create({
     baseURL: apiBaseUrl,
     timeout: 1000,
-});
+  
+})
 
 /**
  * 第一次的时候注册
  */
 export const postRegister = data => new Promise((resolve, reject) => {
+    console.log(qs.stringify(data))
     instance({
         url: '/register',
         method: 'post',
-        data
+        data: qs.stringify(data),
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+
     }).then((res) => {
         const { data: result } = res
         if (result.code === 0){
@@ -30,8 +35,6 @@ export const postRegister = data => new Promise((resolve, reject) => {
 /**
  * 统计用户的使用情况
  */
-export const getStatisticActive = uid => instance.get('/statistic', {
-    params: {
-        uid
-    }
-})
+export const apiStatisticActive = uid => instance.post('/active', qs.stringify({
+    uid
+}), { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
