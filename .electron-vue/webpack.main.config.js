f2173b3e753ef7862805067c34b2c60d3e911692
yangpeng <aitexiaoy@163.com>
2019-07-08 08:55:28 +0800

    feat(main-config): 自改配置文件

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -7,7 +7,7 @@ const { dependencies } = require('../package.json')
 const webpack = require('webpack')
 const fs= require('fs')
 // 检测是否有用户配置文件，没有的话根据模板生成一个
-const userConfigPath=path.resolve(__dirname,'../.user-config1.js')
+const userConfigPath=path.resolve(__dirname,'../.user-config.js')
 if(fs.existsSync(userConfigPath)){
   fs.copyFileSync(path.resolve(__dirname,'../user-config.template.js'), userConfigPath)
 }
